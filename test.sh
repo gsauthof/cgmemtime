c134e9d7b09a1ab085ab040f611d0c3b21c0463d
Georg Sauthoff <mail@georg.so>
2012-12-06 11:36:27 +0100

    add tests

---
+++ test.sh
@@ -0,0 +1,126 @@
+
+set -e
+set -u
+
+TIME=/usr/bin/time
+CGMEMTIME=./cgmemtime
+TESTA=./testa
+
+test_linux()
+{
+  if uname -o | grep -q Linux ; then
+    true
+  else
+    echo -n Not a Linux system
+    return 1
+  fi
+  return 0
+}
+
+test_time()
+{
+  if $TIME --verbose true 2>&1 | grep -q 'Maximum resident set size' ; then
+    true
+  else
+    echo -n '(GNU?)' time does not report maximum RSS
+    return 1
+  fi
+  return 0
+}
+
+test_simple()
+{
+  for a in 4 5; do
+  x=`$CGMEMTIME -t $TESTA x 10 2>&1 | grep ';' | cut -d';' -f $a`
+  if [ $((x/1024)) -ne 10 ]; then
+    echo -n "Not 10 MiB (x=$x, a=$a)"
+    return 1
+  fi
+  done
+  return 0
+}
+
+test_accum()
+{
+  o=`$CGMEMTIME -t $TESTA x 10 20 30 40 2>&1 | grep ';'`
+ 
+  x=`echo $o | cut -d';' -f 4`
+  if [ $((x/1024)) -ne 10 ]; then
+    echo -n "Not 10 MiB (x=$x)"
+    return 1
+  fi
+  x=`echo $o | cut -d';' -f 5`
+  if [ $((x/1024)) -ne 100 ]; then
+    echo -n "Not 100 MiB (x=$x)"
+    return 1
+  fi
+  return 0
+}
+
+test_wall()
+{
+  x=`$CGMEMTIME -t sleep 3 2>&1 | grep ';' | cut -d';' -f 3 | cut -d'.' -f1`
+  if [ $x -ne 3 ]; then
+    echo -n "Not 3 seconds wall clock time (x=$x)"
+    return 1
+  fi
+  return 0
+}
+
+test_notfound()
+{
+  $CGMEMTIME ./notfound 2>/dev/null
+  x=$?
+  if [ $x -ne 127 ]; then
+    echo -n "Exit status is not 127 (x=$x)"
+    return 1
+  fi
+  return 0
+}
+
+test_exitstatus()
+{
+  $CGMEMTIME false 2>/dev/null
+  x=$?
+  if [ $x -ne 1 ]; then
+    echo -n "Exit status is not 1 (x=$x)"
+    return 1
+  fi
+  return 0
+}
+
+tests="
+test_linux
+test_time
+test_simple
+test_accum
+test_wall
+test_notfound
+test_exitstatus
+"
+failed=0
+
+set +e
+for i in $tests; do
+  echo -n Executing $i ...
+  $i
+  if [ $? -eq 0 ]; then
+    echo ' OK'
+  else
+    echo ' FAIL'
+    failed=$((failed+1))
+  fi
+done
+set -e
+
+echo ================================================================================
+echo ================================================================================
+if [ $failed -eq 0 ]; then
+  echo '=> Everything went just fine!'
+  exit 0
+else
+  echo '=> '$failed' tests failed'
+  exit 1
+fi
+
+
---
+++ testa.c
@@ -0,0 +1,47 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#include <sys/types.h>
+#include <unistd.h>
+
+
+void fill_some(size_t n, char ch)
+{
+  size_t bytes = n * (size_t) 1024 * (size_t) 1024;
+  char *b = malloc(bytes);
+  if (!b) {
+    fprintf(stderr, "Failed to allocate %zu MiB\n", n);
+    exit(1);
+  }
+  printf("Allocating %zu MiBs\n", n);
+  for (size_t i = 0; i<bytes; i+=1024) {
+    b[i] = ch;
+  }
+  sleep(1);
+}
+
+int main(int argc, char **argv)
+{
+  char ch = argv[1][0];
+
+  printf("Parent PID is %zd\n", (ssize_t)getpid());
+  fill_some(atoi(argv[2]), ch);
+
+  for (int r = 3; r<argc; ++r) {
+    pid_t ret = fork();
+    if (ret < 0) {
+      perror("Fork failed");
+      exit(2);
+    }
+    if (!ret) {
+      printf("New Child: %zd\n", (ssize_t)getpid());
+      fill_some(atoi(argv[r]), ch);
+      exit(0);
+    }
+  }
+  sleep(argc);
+  return 0;
+}
